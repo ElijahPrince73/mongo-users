a0c5e323ef6c10bf47d29d84d01198380b9f7c12
Elijah Prince <elijahprince73@gmail.com>
2018-03-28 17:16:29 -0700

    Validation With a Validator Function

--- src/users.js
+++ src/users.js
@@ -5,6 +5,10 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
 	name: {
 		type: String,
+		validate: {
+			validator: (name) => name.length > 2,
+			message: 'Message must be longer than 2 characters'
+		},
 		required: [true, 'Name is required']
 	},
 	postCount: Number
--- test/validation_test.js
+++ test/validation_test.js
@@ -13,4 +13,17 @@ describe('Validating records', () => {
 		assert(message === 'Name is required')
 		done()
 	});
+
+	it('reqires a username longer than 2 characters', (done) => {
+		const user = new User({
+			name: 'Al'
+		})
+		const validationResult = user.validateSync()
+		const {
+			message
+		} = validationResult.errors.name
+
+		assert(message === 'Message must be longer than 2 characters')
+		done()
+	});
 });
