18a48c7c00b79d6a4ad3d2294d11cd8d6218d454
Elijah Prince <elijahprince73@gmail.com>
2018-03-29 20:59:38 -0700

    Can add subdocuments to an existing record

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const User = require('../src/users');
 
 describe('Subdocument', () => {
-	it('Can create a Subdocument', (done) => {
+	it('Can create a subdocument', (done) => {
 		const joe = new User({
 			name: 'Joe',
 			posts: [{
@@ -18,5 +18,30 @@ describe('Subdocument', () => {
 				assert(user.posts[0].title === 'Just a new title')
 				done()
 			})
+	});
+
+	it('Can add subdocuments to an existing record', (done) => {
+		const joe = new User({
+			name: 'Joe',
+			posts: []
+		});
+
+		joe.save()
+			.then(() => User.findOne({
+				name: 'Joe'
+			}))
+			.then((user) => {
+				user.posts.push({
+					title: 'New Post'
+				});
+				return user.save();
+			})
+			.then(() => User.findOne({
+				name: 'Joe'
+			}))
+			.then((user) => {
+				assert(user.posts[0].title === 'New Post');
+				done();
+			});
 	});
 });
