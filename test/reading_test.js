df02dad84e51fd1075b90cccc9aef3939a9e012f
Elijah Prince <elijahprince73@gmail.com>
2018-04-10 17:30:46 -0700

    added sort function

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,15 +2,23 @@ const assert = require('assert');
 const User = require('../src/users');
 
 describe('Reading User out of the database', () => {
-	let joe;
+	let joe, maria, alex, zach;
 
 	beforeEach((done) => {
+		alex = new User({
+			name: 'Alex'
+		})
 		joe = new User({
 			name: 'Joe'
 		})
-		joe.save().then(() => {
-			done()
+		maria = new User({
+			name: 'Maria'
 		})
+		zach = new User({
+			name: 'Zach'
+		})
+		Promise.all([joe.save(), alex.save(), maria.save(), zach.save()])
+			.then(() => done())
 	});
 
 	it('finds all user with the name of joe', (done) => {
@@ -31,5 +39,23 @@ describe('Reading User out of the database', () => {
 			assert(user.name === 'Joe')
 			done()
 		})
+	});
+	it.only('Can skip and limit the result set', (done) => {
+		User.find({})
+			//	sort all users in a ascending fashion
+			// descending -1 === c, b, a
+			// ascending 1 === a, b, c
+			.sort({
+				name: 1
+			})
+			.skip(1)
+			.limit(2)
+			.then((users) => {
+				console.log(users);
+				assert(users.length === 2)
+				assert(users[0].name === 'Joe')
+				assert(users[1].name === 'Maria')
+				done()
+			})
 	});
 });
